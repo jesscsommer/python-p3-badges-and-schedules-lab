ae2d56fab8a71d67cace8607c7e3cbc3ae5c52ae
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-22 14:09:29 -0700

    Complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    badge_msgs = [badge_maker(name) for name in names]
+    return badge_msgs
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for i in range(len(names)):
+        rooms.append(f'Hello, {names[i]}! You\'ll be assigned to room {i + 1}!')
+    return rooms
+
 
 def printer(names):
-    return None
+    for msg in batch_badge_creator(names):
+        print(msg)
+    for room in assign_rooms(names):
+        print(room)
